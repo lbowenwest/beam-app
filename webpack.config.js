39075299e91e55fd9db929151f2a91e97b4cee83
tweakyllama <l.bowenwest@googlemail.com>
2016-05-06 00:39:06 +0100

    Fixed webpack.config build and dev serving

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,40 @@ var path = require('path');
 var webpack = require('webpack');
 
 var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var rootAssetPath = path.join(__dirname, 'client');
 
 
 module.exports = {
     entry: {
-        vendor: ['jquery', 'react', 'react-router', 'react-dom'],
-        app: path.join(rootAssetPath, 'src', 'index.js'),
-        style: path.join(rootAssetPath, 'sass', 'index.scss')
+        vendor: [],
+
+        app: path.join(rootAssetPath, 'app'),
+        site: path.join(rootAssetPath, 'site')
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: '/assets/',
+        publicPath: 'http://localhost:8080/assets/',
         filename: '[name].bundle.js',
         chunkFilename: '[id].bundle.js'
     },
     resolve: {
-        extensions: ['', '.js', '.scss', '.css']
+        extensions: ['', '.jsx', '.js', '.scss', '.css']
     },
     resolveLoader: {
         root: path.join(__dirname, 'node_modules')
     },
 
     plugins: [
-        // new ExtractTextPlugin('style.bundle.css'),
+        new ExtractTextPlugin('[name].bundle.css'),
         new ManifestRevisionPlugin(path.join('build', 'manifest.json'), {
-            rootAssetPath: path.join('client'),
-            ignorePaths: ['sass', 'src', 'dist']
+            rootAssetPath: path.join('client')
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             filename: 'vendor.bundle.js',
-            minChunks: Infinity
+            minChunks: 2
         })
     ],
 
@@ -43,20 +43,19 @@ module.exports = {
         loaders: [{
             test: /\.js/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
-            include: [
-                path.join(rootAssetPath, 'src')
-            ]
+            loader: 'babel-loader'
         }, {
-            test: /\.scss/,
-            // loader: ExtractTextPlugin.extract('style', 'css', 'sass'),
-            loaders: ['style', 'css', 'sass'],
-            include: [
-                path.join(rootAssetPath, 'sass')
-            ]
+            test: /\.(sc|sa|c)ss/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass'),
         }, {
             test: /\.json$/,
             loader: 'json-loader'
+        }, {
+            test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url?limit=10000"
+        }, {
+            test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+            loader: 'file'
         }]
     }
 };
